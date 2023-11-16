ef572d8d21481ab47af8d3d9e50c2a04dc51e3c3
Hafeefa <hafeefapc2003@gmail.com>
2023-11-17 01:00:21 +0530

    home

---
+++ Client/Client_pwa/src/components/classbox/classbox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function classbox() {
+  return (
+    
+    <div className=' flex flex-row w-[10rem] h-[7rem] bg-gray-500 bg-opacity-50 mt-[5rem] ml-[-10.5rem]  rounded-3xl text-white'>
+      <h6 className='ml-2 mt-2 font-semibold text-base'>Class A</h6>
+
+  </div>
+  )
+}
--- Client/Client_pwa/src/pages/Otp/Otp.jsx
+++ Client/Client_pwa/src/pages/Otp/Otp.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import bg from '../../assets/images/bg.png'
-import Otpbox from '../../components/Otp/Otpbox'
-import {IoMdArrowRoundBack} from 'react-icons/io';
+// import React from 'react'
+// import bg from '../../assets/images/bg.png'
+// import Otpbox from '../../components/Otp/Otpbox'
+// import {IoMdArrowRoundBack} from 'react-icons/io';
 
 
 
-export default function Otp() {
-  return (
-    <div>
-      <div className='flex justify-center items-center flex-col'>
-        <div className="relative">
-          <img src={bg} alt="background" />
+// export default function Otp() {
+//   return (
+//     <div>
+//       <div className='flex justify-center items-center flex-col'>
+//         <div className="relative">
+//           <img src={bg} alt="background" />
           
-        </div>
+//         </div>
 
-        <div className='relative h-screen/2 w-screen bg-white flex flex-col justify-center items-center'>
-          <div className='!mr-[20rem] !mt-3'>
-            <IoMdArrowRoundBack size={40}  />
-          </div>
-          <div className='flex flex-col items-center justify-center gap-y-3'>
-            <h1 className='font-bold text-2xl text-teal-500 '>Enter OTP</h1>
-            <p>We have send an OTP to your mobile number</p>
-          </div>
-          <div className='mt-[2rem]'>
-            <Otpbox  />
-          </div>
-          <div className='mt-[3rem]'>
-            <button
-              className='h-[52px] w-[335px] rounded-lg font-bold font-inter bg-teal-500  text-white'
+//         <div className='relative h-screen/2 w-screen bg-white flex flex-col justify-center items-center'>
+//           <div className='!mr-[20rem] !mt-3'>
+//             <IoMdArrowRoundBack size={40}  />
+//           </div>
+//           <div className='flex flex-col items-center justify-center gap-y-3'>
+//             <h1 className='font-bold text-2xl text-teal-500 '>Enter OTP</h1>
+//             <p>We have send an OTP to your mobile number</p>
+//           </div>
+//           <div className='mt-[2rem]'>
+//             <Otpbox  />
+//           </div>
+//           <div className='mt-[3rem]'>
+//             <button
+//               className='h-[52px] w-[335px] rounded-lg font-bold font-inter bg-teal-500  text-white'
               
-            >
-              Sign In
-            </button>
+//             >
+//               Sign In
+//             </button>
 
-            <div className="mt-5 flex flex-col justify-center items-center">
-              <p className="text-gray-600 inline">Did'nt recieve the otp?</p>
-              <p className="text-green-600 inline">Resend</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+//             <div className="mt-5 flex flex-col justify-center items-center">
+//               <p className="text-gray-600 inline">Did'nt recieve the otp?</p>
+//               <p className="text-green-600 inline">Resend</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
