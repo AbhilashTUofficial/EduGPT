5693b2e43f15fde1c09b4965c33ccfe893c320a3
i0dheerajdileep <i.dheerajdileep@gmail.com>
2023-11-17 01:00:32 +0530

    login error 50%

--- Client/Client_pwa/src/pages/Otp/Otp.jsx
+++ Client/Client_pwa/src/pages/Otp/Otp.jsx
@@ -1,34 +1,47 @@
-import React from 'react'
-import bg from '../../assets/images/bg.png'
-import Otpbox from '../../components/Otp/Otpbox'
-import {IoMdArrowRoundBack} from 'react-icons/io';
+import React, { useState } from 'react';
+import bg from '../../assets/bg.png'
+// import logo from '../../assets/logo.png'
+import Otpbox from '../../components/otp/Otpbox'
+import { IoMdArrowRoundBack } from 'react-icons/io';
+import {useNavigate} from 'react-router-dom'
 
+function Otp({ handleLoginchange }) {
+  const [otp, setOtp] = useState("");
+  const navigate = useNavigate()
 
+  const handleOtpChange = (value) => {
+    setOtp(value);
+  };
 
-export default function Otp() {
+  const handleOtpVerification = () => {
+    
+  };
+
+  const isOtpFilled = true;
+
   return (
     <div>
       <div className='flex justify-center items-center flex-col'>
         <div className="relative">
           <img src={bg} alt="background" />
-          
+          {/* <img src={logo} alt="logo" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" /> */}
         </div>
 
         <div className='relative h-screen/2 w-screen bg-white flex flex-col justify-center items-center'>
           <div className='!mr-[20rem] !mt-3'>
-            <IoMdArrowRoundBack size={40}  />
+            <IoMdArrowRoundBack size={40} onClick={handleLoginchange} />
           </div>
           <div className='flex flex-col items-center justify-center gap-y-3'>
             <h1 className='font-bold text-2xl text-teal-500 '>Enter OTP</h1>
             <p>We have send an OTP to your mobile number</p>
           </div>
           <div className='mt-[2rem]'>
-            <Otpbox  />
+            <Otpbox otp={otp} handleOtpChange={handleOtpChange} />
           </div>
           <div className='mt-[3rem]'>
             <button
               className='h-[52px] w-[335px] rounded-lg font-bold font-inter bg-teal-500  text-white'
-              
+              onClick={handleOtpVerification}
             >
               Sign In
             </button>
@@ -41,5 +54,7 @@ export default function Otp() {
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+export default Otp;
