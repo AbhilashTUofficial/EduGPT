8718b56d6e13e9e0e653ed7da0f5d5283ae11aff
Hafeefa <hafeefapc2003@gmail.com>
2023-11-17 02:11:05 +0530

    image

--- Client/Client_pwa/src/App.jsx
+++ Client/Client_pwa/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/Otp" element={<Otp />} />
+        <Route path="/Test" element={<Test />} />
       </Routes>
     </>
   );
---
+++ Client/Client_pwa/src/pages/Test/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function test() {
+  return (
+    <div>
+      <form >
+        <h3 className='font-bold  text-teal-500'>Test Name</h3>
+        <input type='text' className='rounded' />
+
+      </form>
+    </div>
+  )
+}
