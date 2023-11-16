d425c4534cf0b03993a4f1daf6011fe0119b5d90
i0dheerajdileep <i.dheerajdileep@gmail.com>
2023-11-17 03:11:26 +0530

    chartjs integ

--- Client/Client_pwa/src/App.jsx
+++ Client/Client_pwa/src/App.jsx
@@ -5,13 +5,10 @@ import Home from "./pages/Home/home";
 import Otp from "./pages/Otp/Otp";
 import Landing from "./pages/Landing/Landing";
 import Login from "./pages/Login/Login";
-<<<<<<< HEAD
 import SetData from "./pages/SetData/SetData";
-=======
 import Test from "./pages/Test/Test";
 import Progress from "./pages/Progress/Progress";
 import ProgressChart from "./pages/ProgressChart/ProgressChart";
->>>>>>> 747d756da60145dd4ac4ab59ee9a2a195bb87a3e
 
 function App() {
   const [count, setCount] = useState(0);
@@ -23,14 +20,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/Otp" element={<Otp />} />
-<<<<<<< HEAD
         <Route path="/setdata" element={<SetData />} />
         {/* <Route path="/Test" element={<Test />} /> */}
-=======
         <Route path="/Test" element={<Test />} />
         <Route path="/progress" element={<Progress/>} />
         <Route path="/progress/:id" element={<ProgressChart/>} />
->>>>>>> 747d756da60145dd4ac4ab59ee9a2a195bb87a3e
       </Routes>
     </>
   );
